3efdc86f63062b6161c367350c418aa70f9b5d73
Mariusz Rokita <mariusz.rokita@gmail.com>
2019-11-15 09:29:59 +0100

    Linter fixes

--- startup.py
+++ startup.py
@@ -1,4 +1,4 @@
-""" 
+"""
 In this sample, the Flask app object is contained within the hello_app *module*;
 that is, hello_app contains an __init__.py along with relative imports. Because
 of this structure, a file like webapp.py cannot be run directly as the startup
@@ -9,4 +9,4 @@ startup.py, that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
 """
 
-from hello_app.webapp import app
+from hello_app.webapp import app  # noqa F401
